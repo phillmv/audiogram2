e49908b2b38ca282e1a7a2c7bcc0844fd3271264
Phill MV <phillmv@okayfail.com>
2013-03-22 12:30:58 -0400

    Added new tags for actionsbrill

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,12 +1,12 @@
 Feed::CLIENT_ID = ENV["CLIENT_ID"] || "822b668bc4ec470b9dc0c31cefcd9119"
 Feed::CLIENT_SECRET = ENV["CLIENT_SECRET"] || "822b668bc4ec470b9dc0c31cefcd9119"
 
-default_tags = [ "longwinterto", 
-                 "winter", 
-                 "snow", 
-                 "toronto", 
-                 "davidcomestolife" ]
-search_tags = [ "longwinterto" ] 
+default_tags = [ "thedesignersrepublic", 
+                 "waterway", 
+                 "naritaexpress",
+                 "arlandaexpress",
+                 "toronto" ]
+search_tags = [ "actionsbrill" ] 
 
 Feed::DEFAULT_TAGS = ENV["DEFAULT_TAGS"] || default_tags
 Feed::SEARCH_TAGS = ENV["SEARCH_TAGS"] || search_tags
